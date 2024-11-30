901b7cf4e6b06ee18609d6464cdc5b50728a3eb0
Ike Rowland <63517484+IkeRowland@users.noreply.github.com>
2024-11-30 18:32:34 +0200

    Add main page component

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import JobSearch from '../components/JobSearch';
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-gray-50">
+      <JobSearch />
+    </main>
+  );
+}
